2c777f4aa335e9bf7effc756273699df9aa25ea3
o0ragman0o <o0ragman0o+git@gmail.com>
2016-06-29 17:26:24 +1000

    day out

--- ITT.sol
+++ ITT.sol
@@ -21,6 +21,184 @@
 	bool mutex;
 }
 
+
+contract MultiCircularLinkedList 
+{
+/* Modifiers */
+
+	// To test if mapping keys point to a valid linked list node.
+	modifier keyExists(uint _listKey, uint _nodeKey) { 
+		if (linkedLists[_listKey].nodes[_nodeKey].dataIndex == 0) throw; _
+	}
+
+	modifier isValidData(uint _dataIdx) {
+		if (_dataIdx == 0) throw; _
+	}
+
+	modifier isNotEmpty(uint _listKey) {
+		if (linkedLists[_listKey].size == 0) throw; _
+	}
+	
+/* Structs */
+
+	// Generic double linked list node.
+	struct DoubleLinkNode {
+		uint dataIndex;
+		uint next;
+		uint prev;
+	}
+	
+	// Generic circular linked list parameters. Head is static index 1.
+	struct LinkedList {
+		uint size;	// Number of nodes
+		uint newNodeKey; // Next free mapping slot
+		uint auxData; // auxilary data state variable.
+		mapping (uint => DoubleLinkNode) nodes;
+	}
+	
+/* Constants */
+
+	uint constant HEAD = 0; // All linked lists are circular with static head.
+	bool constant PREV = false;
+	bool constant NEXT = true;
+
+/* State valiables */
+
+	// `linkedLists` is a mapping to store all circular linked lists.
+	// The `dataIndex` of each node in the index list (key == 0) is the key for a
+	// a child linked list, e.g:
+	//
+	//     linkedLists[0].dataIndex == 100 --> linkedLists[100]
+	//
+	// The dataIndex of a child list (key !=0) can be use as a key or index for
+	// an array or mapping, e.g.
+	//
+	//     linkedLists[100].dataIndex == 123 --> data[123]
+	//
+
+	mapping (uint => LinkedList) public linkedLists;  
+
+/* State variable Accessor Functions (For reference only. Leave commented)
+
+	function linkedLists(uint listKey, uint nodeKey, uint DATAINDEX) returns (uint) // dataIndex
+	function linkedLists(uint listKey, uint nodeKey, uint NEXT) returns (uint) // next
+	function linkedLists(uint listKey, uint nodeKey, uint PREV) returns (uint) // prev
+*/
+
+/* Functions */
+
+	// Initialises circular linked list to a valid state
+	function initLinkedList(uint _listKey, bool _reset) 
+		returns (bool)
+//		internal
+	{
+		if (linkedLists[_listKey].nodes[HEAD].dataIndex != 0 && !_reset) return true;
+		linkedLists[_listKey].newNodeKey = 1; // key 1 is already head
+		linkedLists[_listKey].nodes[HEAD].next = HEAD; // set next link to head
+		linkedLists[_listKey].nodes[HEAD].prev = HEAD; // set previous link to head
+		linkedLists[_listKey].nodes[HEAD].dataIndex = 1;
+		return true;
+	}
+
+	function insertBefore(uint _listKey, uint _nodeKey, uint _dataIndex)
+//		internal
+		keyExists(_listKey, _nodeKey)
+		isValidData(_dataIndex)
+		returns (uint)
+	{
+		LinkedList list = linkedLists[_listKey];
+		DoubleLinkNode newNode = list.nodes[list.newNodeKey];
+		newNode.next = _nodeKey;
+		newNode.prev = list.nodes[_nodeKey].prev;
+		list.nodes[list.nodes[_nodeKey].prev].next = list.newNodeKey;
+		list.nodes[_nodeKey].prev = list.newNodeKey;
+		newNode.dataIndex = _dataIndex; // Store new data.
+		list.size++;
+		list.newNodeKey++;
+		return list.newNodeKey - 1;
+	}
+
+	function insertAfter(uint _listKey, uint _nodeKey, uint _dataIndex)
+//		internal
+		keyExists(_listKey, _nodeKey)
+		isValidData(_dataIndex)
+		returns (uint)
+	{
+		LinkedList list = linkedLists[_listKey];
+		DoubleLinkNode newNode = list.nodes[list.newNodeKey];
+		newNode.prev = _nodeKey;
+		newNode.next = list.nodes[_nodeKey].next;
+		list.nodes[list.nodes[_nodeKey].next].prev = list.newNodeKey;
+		list.nodes[_nodeKey].next = list.newNodeKey;
+		newNode.dataIndex = _dataIndex; // Store new data.
+		list.size++;
+		list.newNodeKey++;
+		return list.newNodeKey - 1;
+	}
+	
+	function update(uint _listKey, uint _nodeKey, uint _dataIndex)
+//		internal
+		keyExists(_listKey, _nodeKey)
+		returns (bool)
+	{
+		linkedLists[_listKey].nodes[_nodeKey].dataIndex = _dataIndex;
+		return true;
+	}
+		
+	function remove(uint _listKey, uint _nodeKey)
+//		internal
+		keyExists(_listKey, _nodeKey)
+		isNotEmpty(_listKey)
+		returns (bool)
+	{
+		LinkedList list = linkedLists[_listKey];
+		list.nodes[list.nodes[_nodeKey].prev].next = list.nodes[_nodeKey].next;
+		list.nodes[list.nodes[_nodeKey].next].prev = list.nodes[_nodeKey].prev;
+		list.size--;
+		delete list.nodes[_nodeKey];  // delete the node.
+		return true;
+	}
+	
+	function stepPrev(uint _listKey, uint _nodeKey)
+		keyExists(_listKey, _nodeKey)
+		constant returns (uint)
+	{
+		return linkedLists[_listKey].nodes[_nodeKey].next;
+	}
+
+	function stepNext(uint _listKey, uint _nodeKey)
+		keyExists(_listKey, _nodeKey)
+		constant returns (uint)
+	{
+		return linkedLists[_listKey].nodes[_nodeKey].prev;
+	}
+
+	function seekUp(uint _listKey, uint _start, uint _target)
+		keyExists(_listKey, _start)
+		constant returns (uint)
+	{
+		LinkedList list = linkedLists[_listKey];
+		while(list.nodes[_start].next < _target) _start = list.nodes[_start].next;
+		return _start;
+	}
+
+	function seekDown(uint _listKey, uint _start, uint _target)
+		keyExists(_listKey, _start)
+		constant returns (uint)
+	{
+		LinkedList list = linkedLists[_listKey];
+		while(list.nodes[_start].prev > _target) _start = list.nodes[_start].prev;
+		return _start;
+	}
+
+	function isValidKey(uint _listKey, uint _nodeKey) 
+		constant
+		returns (bool)
+	{ 
+		if (linkedLists[_listKey].nodes[_nodeKey].dataIndex > 0) 
+			return true;
+	}
+}
 // EIP20 Standard Token Interface
 
 
@@ -133,149 +311,24 @@ contract EIP20Token is EIP20Interface
 }
 
 
-contract MultiCircularLinkedList 
-{
-/* Modifiers */
-
-	// To test if mapping keys point to a valid linked list node.
-	modifier isValidKey(uint _listKey, uint _nodeKey) { 
-		if (linkedLists[_listKey].nodes[_nodeKey].next == 0) throw; _
-	}
-
-	modifier isNotEmpty(uint _listKey) {
-		if (linkedLists[_listKey].size == 0) throw; _
-	}
-	
-	// modifier stepDirection(bool _dir) {
-	// 	uint NEXT = 1;
-	// 	uint PREV = 2;
-	// 	if (_dir) {
-	// 		NEXT = 2;
-	// 		PREV = 1;
-	// 	}
-	// }
-
-/* Structs */
-
-	// Generic double linked list node.
-	struct DoubleLinkNode {
-		uint dataIndex;
-		uint next;
-		uint prev;
-	}
-	
-	// Generic circular linked list parameters. Head is static index 1.
-	struct LinkedList {
-		uint size;	// Number of nodes
-		uint newNodeKey; // Next free mapping slot
-		uint auxData; // auxilary data state variable.
-		mapping (uint => DoubleLinkNode) nodes;
-	}
-	
-/* Constants */
-
-	uint constant HEAD = 1; // All linked lists are circular with static head.
-	uint constant DATAINDEX = 0;
-
-/* State valiables */
-
-	// `linkedLists` is a mapping to store all circular linked lists.
-	// The `dataIndex` of each node in the index list (key == 0) is the key for a
-	// a child linked list, e.g:
-	//
-	//     linkedLists[0].dataIndex == 100 --> linkedLists[100]
-	//
-	// The dataIndex of a child list (key !=0) can be use as a key or index for
-	// an array or mapping, e.g.
-	//
-	//     linkedLists[100].dataIndex == 123 --> data[123]
-	//
-
-	mapping (uint => LinkedList) linkedLists;  
-
-/* State variable Accessor Functions (For reference only. Leave commented)
-
-	function linkedLists(uint listKey, uint nodeKey, uint DATAINDEX) returns (uint) // dataIndex
-	function linkedLists(uint listKey, uint nodeKey, uint NEXT) returns (uint) // next
-	function linkedLists(uint listKey, uint nodeKey, uint PREV) returns (uint) // prev
-*/
-
-/* Functions */
-
-	// Initialises circular linked list to a valid state
-	function initLinkedList(uint _listKey, bool _reset)
-		internal
-	{
-		if (linkedLists[_listKey].nodes[HEAD].next != 0 && !_reset) return;
-		linkedLists[_listKey].newNodeKey = 2; // key 1 is already head
-		linkedLists[_listKey].nodes[1].next = HEAD; // set next link to head
-		linkedLists[_listKey].nodes[1].prev = HEAD; // set previous link to head
-	}
-
-	// Inserts new node before or after node key.
-	function insertNode(uint _listKey, uint _nodeKey, uint _dataIndex, bool _dir)
-		isValidKey(_listKey, _nodeKey)
-		returns (uint)
-	{
-		LinkedList list = linkedLists[_listKey];
-		DoubleLinkNode newNode = list.nodes[list.newNodeKey];
-		if (_dir) {
-			// Insert before node.
-			newNode.next = _nodeKey;
-			newNode.prev = list.nodes[_nodeKey].prev;
-			list.nodes[list.nodes[_nodeKey].prev].next = list.newNodeKey;
-			list.nodes[_nodeKey].prev = list.newNodeKey;
-		} else {
-			// Insert after node.
-			newNode.prev = _nodeKey;
-			newNode.next = list.nodes[_nodeKey].next;
-			list.nodes[list.nodes[_nodeKey].next].prev = list.newNodeKey;
-			list.nodes[_nodeKey].next = list.newNodeKey;
-		}
-		newNode.dataIndex = _dataIndex; // Store new data.
-		list.size++;
-		list.newNodeKey++;
-		return list.newNodeKey - 1;
-	}
-	
-	function update(uint _listKey, uint _nodeKey, uint _dataIndex)
-		isValidKey(_listKey, _nodeKey)
-		returns (bool)
-	{
-		linkedLists[_listKey].nodes[_nodeKey].dataIndex = _dataIndex;
-		return true;
-	}
-		
-	function remove(uint _listKey, uint _nodeKey)
-		isValidKey(_listKey, _nodeKey)
-		isNotEmpty(_listKey)
-		returns (bool)
-	{
-		LinkedList list = linkedLists[_listKey];
-		list.nodes[list.nodes[_nodeKey].prev].next = list.nodes[_nodeKey].next;
-		list.nodes[list.nodes[_nodeKey].next].prev = list.nodes[_nodeKey].prev;
-		list.size--;
-		delete list.nodes[_nodeKey];  // delete the node.
-		return true;
-	}
-	
-	function step(uint _listKey, uint _nodeKey, bool _dir)
-//	function step(uint _listKey, uint _nodeKey, uint _dir)
-		isValidKey(_listKey, _nodeKey)
-		constant returns (uint)
-	{
-//		return linkedLists(_listKey,_nodeKey, _dir);
-		 if (_dir) return linkedLists[_listKey].nodes[_nodeKey].prev;
-		else return linkedLists[_listKey].nodes[_nodeKey].next;
-	}
-}
-
-
 /* Intrinsically Tradable Token code */	
 contract ITT is EIP20Token, MultiCircularLinkedList
 {
 
 /* Modifiers */
+	modifier isValidBuy(uint _bidPrice) {
+		if (msg.value == 0 || _bidPrice == 0) throw; _	// has nothing to spend
+	}
+
+	modifier isValidSell(uint _askPrice, uint _amount) {
+		if (_amount > balances[msg.sender] ||
+			_askPrice == 0 ||
+			_amount == 0) throw; _
+	}
+
+	modifier ownsOrder(uint _orderId) {
+		if (msg.sender != orders[_orderId].trader) throw; _		
+	}
 
 /* Structs */
 
@@ -293,7 +346,10 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 	bool constant ASK = true;
 	bool constant MAKE = false;
 	bool constant TAKE = true;
-	uint constant PRICEBOOK = 0;
+	bool constant NO_RESET = false;
+	bool constant RESET = true;
+	uint constant PRICE_BOOK = 0;
+
 
 
 /* State Valiables */
@@ -307,7 +363,9 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 /* Functions */
 
 	function ITT() {
+		totalSupply = 1000000;
 		balances[msg.sender] = 1000000;
+		linkedLists[0].nodes[0].dataIndex = 1;
 		}
 	
 	function () 
@@ -318,31 +376,54 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 		
 /* Functions Getters */
 
+	function getMetrics()
+		constant
+		returns (
+			uint _balance,
+			uint _available,
+			uint _lowestAsk,
+			uint _highestBid,
+			uint _askVol,
+			uint _bidVol
+		)
+	{
+		_balance = balances[msg.sender];
+		_available = _balance - unavailable[msg.sender];
+		_lowestAsk = lowestAsk();
+		_highestBid = highestBid();
+		_askVol = linkedLists[_lowestAsk].auxData;
+		_bidVol = linkedLists[_highestBid].auxData;
+		return;
+	}
+
 	function getVolumeAtPrice(uint _price)
 		constant
-		returns (uint _volume)
+		returns (uint)
 	{
-		_volume = linkedLists[PRICEBOOK].auxData;
+		return linkedLists[PRICE_BOOK].auxData;
 	}
 	
-	function highestBid() constant returns(uint)
+	function highestBid() 
+		constant
+		returns(uint)
 	{
-		return step(PRICEBOOK, HEAD, BID);
+		return stepPrev(PRICE_BOOK, HEAD);
 	}
 	
-	function lowestAsk() constant returns(uint)
+	function lowestAsk() 
+		constant 
+		returns(uint)
 	{
-		return step(PRICEBOOK, HEAD, ASK);
+		return stepNext(PRICE_BOOK, HEAD);
 	}
 
 /* Functions Public */
 
 	function buy (uint _bidPrice, bool _make) public
 		isProtected
+		isValidBuy(_bidPrice)
 		returns (bool _success, uint _spent, uint _ordId)
 	{
-		if (msg.value == 0 ||
-			_bidPrice == 0) throw;	// has nothing to spend
 
 		uint ethRemaining = msg.value; 
 		_spent = ethRemaining; // tally to return unspent ether.
@@ -355,9 +436,7 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 
 		if (ethRemaining > 0 && _make) {
 			// Make 'Buy' order with leftover ether.
-			_ordId = makeBid(_bidPrice, ethRemaining / _bidPrice);
-		} else {
-			_ordId = 0;
+			_ordId = make(_bidPrice, ethRemaining / _bidPrice, BID);
 		}
 
 		if (ethRemaining > 0 && !_make) {
@@ -365,19 +444,15 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 			// *ANALYSIS REQ* Is this prone to ether bootstrap failure?
 			if(!msg.sender.send(ethRemaining)) throw;
 		}
-
 		_spent -= ethRemaining;
 		_success = true;
 	}
 
 	function sell (uint _askPrice, uint _amount, bool _make) public 
 		isProtected
+		isValidSell(_askPrice, _amount)
 		returns (bool _success, uint _sold, uint _ordId)
 	{
-		if (_amount > balances[msg.sender] ||
-			_askPrice == 0 ||
-			_amount == 0) throw;
-
 		uint amountRemaining = _amount;
 		while (_askPrice <= highestBid() && amountRemaining > 0)	{
 			// Take highest bid order above the ask price.
@@ -387,27 +462,25 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 
 		if (amountRemaining > 0 && _make) {
 			// Make 'Sell' order with remaining amount.
-			_ordId = makeAsk(_askPrice, amountRemaining);
-		} else {
-			_ordId = 0;
+			_ordId = make(_askPrice, amountRemaining, ASK);
 		}
 		_sold -= _amount;
 		_success = true;
 	}
 
-	function cancel(uint _orderId) public
+	function cancelOrder(uint _orderId) public
 		isProtected
+		ownsOrder(_orderId)
 		returns (bool _success)
 	{
-		if (msg.sender != orders[_orderId].trader) throw;
 		closeOrder(_orderId);
 		_success = true;
 	}
 
 /* Functions Internal */
 
-	function openOrder(uint _price, uint _amount, bool _swap)
-		internal
+	function make(uint _price, uint _amount, bool _swap)
+//		internal
 		returns (uint _orderId)
 	{
 		Order order = orders[orders.length];
@@ -415,30 +488,14 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 		order.trader = msg.sender;
 		order.price = _price;
 		order.amount = _amount;
-		initLinkedList(_price, false); // ensure price
-		insertNode(_price, HEAD, _orderId, MAKE);
-		linkedLists[_price].auxData += _amount; // update volume
+		insertPrice(_price, _swap); // setup FIFO in price book
+		insertBefore(_price, HEAD, _orderId); // insert order into FIFO
+		addVolume(_price, _amount);
 		return _orderId;		
-	}
-
-	function makeBid(uint _price, uint _amount)
-		internal
-		returns (uint _ordId)
-	{
-		_ordId = openOrder(_price, _amount, BID);
-		return;
-	}
-
-	function makeAsk(uint _price, uint _amount)
-		internal
-		returns (uint _ordId)
-	{
-		_ordId = openOrder(_price, _amount, ASK);
-		return;
 	}
 	
 	function takeAsk(uint _ether)
-		internal
+//		internal
 		returns (uint _ethRemaining)
 	{
 		Order order = orders[lowestAsk()];
@@ -456,7 +513,7 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 	}
 	
 	function takeBid(uint _amount)
-		internal
+//		internal
 		returns (uint _amountRemaining)
 	{
 		Order order = orders[highestBid()];
@@ -473,20 +530,20 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 	}
 	
 	function closeOrder(uint _orderId)
-		internal
+//		internal
 		returns (bool)
 	{
 		uint price = orders[_orderId].price;
 		remove(price, _orderId);
 		linkedLists[price].auxData -= orders[_orderId].amount;
 		if (linkedLists[orders[_orderId].price].size == 0)
-			remove(PRICEBOOK, price);
+			remove(PRICE_BOOK, price);
 		delete orders[_orderId];
 		return true;
 	}
 		
 	function swap(address _seller, address _buyer, uint _price, uint _amount)
-		internal
+//		internal
 		returns (bool)
 	{
 		if (balances[_seller] < _amount) throw;
@@ -495,5 +552,42 @@ contract ITT is EIP20Token, MultiCircularLinkedList
 		if(!_seller.send(_price * _amount)) throw;
 		balances[_buyer] += _amount;
 		return true;
+	}
+
+	function addVolume(uint _price, uint _amount) 
+		internal
+	{
+		linkedLists[_price].auxData += _amount; // update volume		
+	}
+
+	function subVolume(uint _price, uint _amount) 
+		internal
+	{
+		linkedLists[_price].auxData -= _amount; // update volume		
+	}
+
+	function insertPrice(uint _price, bool _swap)
+//		internal
+		returns (uint)
+	{
+		initLinkedList(_price, NO_RESET); // in Price_book
+
+		if (_swap) {
+			// Is selling
+			if (_price < lowestAsk())
+				// is lowest ask
+				return insertAfter(PRICE_BOOK, 0, _price);
+			else
+				// is higher than lowest ask 
+				return insertAfter(PRICE_BOOK, seekUp(PRICE_BOOK, lowestAsk(), _price), _price);			
+		} else {
+			// Is buying
+			if (_price > highestBid())
+				// is highest bid
+				return insertBefore(PRICE_BOOK, 0, _price);
+			else
+				// is lower than highest bid
+				return insertBefore(PRICE_BOOK, seekDown(PRICE_BOOK, highestBid(), _price), _price);
+		}
 	}
 }
