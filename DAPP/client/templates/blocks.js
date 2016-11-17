d0da385d0644e2deb4a8be663a388c5e6a31ee77
o0ragman0o <o0ragman0o+git@gmail.com>
2016-11-17 14:02:44 +1000

    Removed DAPP into seperate project

--- DAPP/client/templates/blocks.js
+++
@@ -1,7 +0,0 @@
-import "./blocks.html";
-
-Template.blocks.helpers ({
-	'block': function () { return ittDict["latestBlock"].get(); },
-	'bestBlock': function () {},
-	'chainId': function () { return web3.version.network}
-})
